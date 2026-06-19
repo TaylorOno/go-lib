97ba088df8204c08183f6e0c53d122664eb3e3e9
TaylorOno <Taylor.R.Ono@gmail.com>
2026-06-19 10:55:43 -0600

    enable tls support

--- web/server.go
+++ web/server.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"flag"
 	"fmt"
@@ -17,6 +18,8 @@ import (
 var (
 	port      string
 	debugPort string
+
+	defaultShutdownTimeout = 10 * time.Second
 )
 
 func init() {
@@ -30,6 +33,10 @@ type Middleware func(next http.HandlerFunc) http.HandlerFunc
 type Server struct {
 	port              string
 	debugPort         string
+	cert              string
+	key               string
+	shutdownTimeout   time.Duration
+	httpServer        *http.Server
 	mux               *http.ServeMux
 	readinessRegistry *healthRegistry
 	livenessRegistry  *healthRegistry
@@ -41,10 +48,12 @@ func NewServer(opts ...OptionFunc) *Server {
 	// default server
 	s := &Server{
 		port:              "8080",
+		shutdownTimeout:   defaultShutdownTimeout,
 		mux:               http.NewServeMux(),
 		readinessRegistry: newHealthRegistry(),
 		livenessRegistry:  newHealthRegistry(),
 		middleware:        []Middleware{},
+		httpServer:        &http.Server{},
 	}
 
 	// apply config overrides
@@ -65,14 +74,39 @@ func (s *Server) HandleFunc(pattern string, handler http.HandlerFunc) {
 	s.mux.HandleFunc(pattern, handler)
 }
 
+// SetIdleTimeout Overrides the default SetIdleTimeout
+func (s *Server) SetIdleTimeout(timeout time.Duration) {
+	s.httpServer.IdleTimeout = timeout
+}
+
+// SetReadTimeout Overrides the default ReadTimeout
+func (s *Server) SetReadTimeout(timeout time.Duration) {
+	s.httpServer.ReadTimeout = timeout
+}
+
+// SetWriteTimeout Overrides the default WriteTimeout
+func (s *Server) SetWriteTimeout(timeout time.Duration) {
+	s.httpServer.WriteTimeout = timeout
+}
+
+// SetCustomTLSConfig Allows setting a custom TLS configuration, avoid using this configuration unless truly needed
+// as it might compromise security if not handled correctly
+func (s *Server) SetCustomTLSConfig(tlsConfig *tls.Config) {
+	s.httpServer.TLSConfig = tlsConfig
+}
+
+// SetCustomServer Overrides the entire http.Server settings, avoid using this configuration unless truly needed
+// as it might compromise security if not handled correctly
+func (s *Server) SetCustomServer(customServer *http.Server) {
+	s.httpServer = customServer
+}
+
 // Start starts the web server with the given context and will block until the context has been canceled. A context cancellation will cause a graceful shutdown.
 func (s *Server) Start(ctx context.Context) error {
 	var err error
 
 	// Configure Server
-	httpServer := &http.Server{
-		Handler: s.mux,
-	}
+	s.httpServer.Handler = s.mux
 
 	listener, err := net.Listen("tcp", net.JoinHostPort("", s.port))
 	if err != nil {
@@ -85,10 +119,7 @@ func (s *Server) Start(ctx context.Context) error {
 
 	// Server loop
 	go func() {
-		slog.Info(fmt.Sprintf("listening on %s\n", listener.Addr()))
-		if err := httpServer.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
-		}
+		s.server(listener)
 	}()
 
 	// Launch pprof if the port has been specified
@@ -103,11 +134,24 @@ func (s *Server) Start(ctx context.Context) error {
 	slog.Info("stopping webserver")
 
 	// Wait for 10 seconds before forcing a shutdown.
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
 	defer cancel()
-	if err = httpServer.Shutdown(shutdownCtx); err != nil {
+	if err = s.httpServer.Shutdown(shutdownCtx); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (s *Server) server(listener net.Listener) {
+	slog.Info(fmt.Sprintf("listening on %s\n", listener.Addr()))
+	if s.cert != "" && s.key != "" {
+		if err := s.httpServer.ServeTLS(listener, s.cert, s.key); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
+		}
+	} else {
+		if err := s.httpServer.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
+		}
+	}
 }
